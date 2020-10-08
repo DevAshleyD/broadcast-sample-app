45dac06460fd0ec7a731125b899b44719a1f5ebf
Ashley D <webdevmvp@gmail.com>
2020-10-08 01:14:05 +0000

    removes postinstall

--- .heroku/post-deploy.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-
-config.apiKey = process.env.apiKey;
-config.apiSecret = process.env.apiSecret;
-
-fs.writeFileSync('./config.json', JSON.stringify(config));
